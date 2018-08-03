f081cb5c29366a93de9a3b97f5c18ed64bbf98ff
Laura Darlak <laura@polyporedesign.com>
2018-08-03 16:39:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  else int % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
-  elsif "Fizz" && "Buzz"
+  else "Fizz" && "Buzz"
     "FizzBuzz"
   end
 end
